b22a972b579df62674dac896793a58b1f4a1be9c
tubagus rivan <tubagus.rivan@inovasisolusi.com>
2023-06-27 16:45:31 +0700

    Initialize branch

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
+import express from 'express'
+import type { Application, Request, Response, NextFunction } from 'express'
 
 const app: Application = express()
-const port: Number = 4000
+const port: number = 4000
 
 app.use('/health', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ status: '200' })
